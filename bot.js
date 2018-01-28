de885d17c736227411d4cbca65567d2d29d08ed1
shakkox <35897237+shakkox@users.noreply.github.com>
2018-01-28 17:52:55 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,6 @@ client.on('ready', () => {
 client.on('message', message => {
    if (message.content === 'ping') {
      message.reply('pong');
-   if (message.content === '%musica') {
-     message.content('https://www.youtube.com/watch?v=2HnEud_VRYY');
    }
 });
 
